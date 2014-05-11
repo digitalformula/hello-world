228d629976190d255a2973b0aa4a89e98393efc2
Chris <chris@digitalformula.net>
2014-05-11 16:22:12 +1000

    Project creation & initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# digitalformula/hello-world
+
+Packagist package for testing inside blog article re Packagist
---
+++ src/DigitalFormula/HelloWorld/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace DigitalFormula\HelloWorld;
+
+class PrintHelloWorld
+{
+    public static function SayIt()
+    {
+        return 'Hello, World!';
+    }
+}
