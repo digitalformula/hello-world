08733c117ee769e38e0813658090f6a56448da34
Chris <chris@digitalformula.net>
2014-05-12 22:27:34 +1000

    Add phpDocumentor comments to HelloWorld.php

--- src/DigitalFormula/HelloWorld/HelloWorld.php
+++ src/DigitalFormula/HelloWorld/HelloWorld.php
@@ -1,11 +1,37 @@
-<?php 
+<?php
+/**
+ *
+ * Contains the PrintHelloWorld class
+ *
+ * @author Chris Rasmussen, digitalformula.net
+ * @category Tests and POC
+ * @license MIT
+ * @package DigitalFormula\HelloWorld
+ *
+ */
 
 namespace DigitalFormula\HelloWorld;
 
+/**
+ *
+ * The PrintHelloWorld class
+ *
+ * @package DigitalFormula\HelloWorld
+ *
+ */
 class PrintHelloWorld
 {
+
+    /**
+     * Print a simple 'Hello, World!' message
+     * @returns string The 'Hello, World!' message
+     */
     public static function SayIt()
     {
         return 'Hello, World!';
     }
+    /* SayIt */
 }
+/* PrintHelloWorld */
+
+?>
